44cab0df9b5c19dff21e61c7aa0546cb4603b6b8
XOTlC <twilightgeometrydash@gmail.com>
2025-02-03 01:34:14 +0000

    update blookifyImage function and add bannerifyImage function with new assets

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export async function blookifyImage(imageBuffer: Buffer): Promise<Buffer> {
-    const BLOOK_MASK_IMAGE_PATH = path.resolve(__dirname, './assets/mask.png');
-    const BLOOK_OVERLAY_IMAGE_PATH = path.resolve(__dirname, './assets/overlay.png');
+    const BLOOK_MASK_IMAGE_PATH = path.resolve(__dirname, './assets/blook-mask.png');
+    const BLOOK_OVERLAY_IMAGE_PATH = path.resolve(__dirname, './assets/blook-overlay.png');
 
     const BLOOK_MASK_IMAGE = await fs.promises.readFile(BLOOK_MASK_IMAGE_PATH);
     const BLOOK_OVERLAY_IMAGE = await fs.promises.readFile(BLOOK_OVERLAY_IMAGE_PATH);
@@ -20,7 +20,7 @@ export async function blookifyImage(imageBuffer: Buffer): Promise<Buffer> {
     const image = await sharp(imageBuffer)
         .ensureAlpha()
         .flatten({ background: { r: 255, g: 255, b: 255 } })
-        .resize(300, 300)
+        .resize(300, 300, { fit: 'fill' })
         .png()
         .toBuffer();
 
@@ -41,10 +41,41 @@ export async function blookifyImage(imageBuffer: Buffer): Promise<Buffer> {
         .png()
         .toBuffer();
 
-    const finalImage = await sharp(maskedImage)
+    return await sharp(maskedImage)
         .composite([{ input: overlay, blend: 'multiply' }])
         .webp({ quality: 90 })
         .toBuffer();
+}
 
-    return finalImage;
+export async function bannerifyImage(imageBuffer: Buffer): Promise<Buffer> {
+    const BANNER_MASK_IMAGE_PATH = path.resolve(__dirname, './assets/banner-mask.png');
+    const BANNER_OVERLAY_IMAGE_PATH = path.resolve(__dirname, './assets/banner-overlay.png');
+
+    const BANNER_MASK_IMAGE = await fs.promises.readFile(BANNER_MASK_IMAGE_PATH);
+    const BANNER_OVERLAY_IMAGE = await fs.promises.readFile(BANNER_OVERLAY_IMAGE_PATH);
+
+    const mask = await sharp(BANNER_MASK_IMAGE)
+        .png()
+        .toBuffer();
+
+    const overlay = await sharp(BANNER_OVERLAY_IMAGE)
+        .png()
+        .toBuffer();
+
+    const image = await sharp(imageBuffer)
+        .ensureAlpha()
+        .flatten({ background: { r: 255, g: 255, b: 255 } })
+        .resize(700, 155, { fit: 'fill' })
+        .png()
+        .toBuffer();
+
+    const maskedImage = await sharp(image)
+        .composite([{ input: mask, blend: 'dest-out' }])
+        .png()
+        .toBuffer();
+
+    return await sharp(maskedImage)
+        .composite([{ input: overlay, blend: 'multiply' }])
+        .webp({ quality: 90 })
+        .toBuffer();
 }
